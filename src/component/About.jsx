2600b6ce42d52825ab3013a9b90c55e6fd6a98cc
Enriquedonaire <donaire.q2@gmail.com>
2023-11-09 18:50:24 +0100

    Add New Description

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -14,19 +14,7 @@ const About = () => {
       <section className="abouttext">
         <div className="text container-fluid">
           <h1>/ ABOUT</h1>
-          <p>John Likens is a director and designer based in New York City. Recognized with winning an Emmy Award
-            for Outstanding Graphic Design and Art Direction, a SXSW Title Design Award, and a spot on Shots’
-            Rising Stars list, he’s well known for crafting memorable visuals.</p>
-          <p>John started his career in New York creating stunning motion graphics for commercial, film, and
-            gaming clients. His passion for film has led him to design cinematic title sequences for Deadpool 2,
-            Marvel’s Eternals, HBO’s “The Night Of,” HBOMax’s “Warrior,” and the award-winning titles for
-            Netflix’s “Godless,” among others.</p>
-          <p>John’s work also includes the holographic UI designs that appear in “Iron Man 3,” “RoboCop,” and
-            “Transformers: Age of Extinction.” He provided design direction for the Netflix documentary “Mission
-            Blue,” and art directed Red Bull’s first feature documentary “Blood Road.” Before Method, John
-            worked as a freelancer in many top NY studios including The Mill and Framestore.</p>
-          <p>If you would like to collaborate on a project, have an inquiry, or just want to say hello – please
-            get in touch here.</p>
+          <p>Highly creative filmmaker gaduated as Digital Cinema & Video Camera and editor Technician, with over 5 years of experience making a variety of shortfilms and advertising videos for diverses freelance companies and independent directors. Strong Passion for creating short films with great stories, but also have a knack for creating commercial productions and documentaries Knowledge of latest cameras, video and audio equipament and editing software. Enjoys working with a variety of scripts, actors and crew members at many different experience levels. in love with visual storytelling, from drawing to film.</p>
         </div>
       </section>
 

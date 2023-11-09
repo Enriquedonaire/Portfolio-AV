84d1e61f0540774946a0a216661426b1f81ea56b
Enriquedonaire <donaire.q2@gmail.com>
2023-11-09 20:55:33 +0100

    Add Style

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,36 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../assets/sass/style.scss'
+import '../assets/sass/style.scss';
+
 const NavBar = () => {
   return (
-    <div>
-      <header className="container-fluid">
-        <div className="row alt-header">
-          <div className="col-lg-6 col-md-6 col-sm-6 p-0">
-            <div className="alt-img" >
-              <Link to="/Portfolio-AV/">
-                <img src={`${process.env.PUBLIC_URL}/img/EnriqueDonaire.png`} width="300" height=""alt="logo" />
-              </Link>
-            </div>
-          </div>
-          <div className="col-lg-6 col-md-6 col-sm-6 p-0">
-            <div className="nav-links">
-              <ul>
-                <li>
-                  <Link to='/Portfolio-AV/'>| Work </Link>
-                </li>
-                <li>
-                  <Link to='/about'>| About</Link>
-                </li>
-                <li>
-                  <Link to='/contact'> | Contact</Link>
-                </li>
-              </ul>
-            </div>
+    <header className="container-fluid">
+      <div className="row alt-header">
+        <div className="col-lg-6 col-md-6 col-sm-6 p-0">
+          <div className="alt-img">
+            <Link to="/Portfolio-AV/">
+              <img
+                src={`${process.env.PUBLIC_URL}/img/EnriqueDonaire.png`}
+                alt="logo"
+                className="logo-img"
+              />
+            </Link>
           </div>
         </div>
-      </header>
-    </div>
+        <div className="col-lg-6 col-md-6 col-sm-6 p-0">
+          <div className="nav-links">
+            <ul>
+              <li>
+                <Link to="/Portfolio-AV/">| Work </Link>
+              </li>
+              <li>
+                <Link to="/about">| About</Link>
+              </li>
+              <li>
+                <Link to="/contact"> | Contact</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
   );
 };
 
